ea2b7ff41cb2755931f0445f871de8c17376c964
emilycharris <emily.harris.mail@gmail.com>
2016-05-18 16:15:56 -0400

    added ignore list

--- my_histogram_hard_mode.py
+++ my_histogram_hard_mode.py
@@ -1,5 +1,16 @@
 import string
 
+ignore_string = "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,"\
+"because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,"\
+"for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,"\
+"it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,"\
+"not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,"\
+"since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,"\
+"twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,"\
+"would,yet,you,your"
+
+ignore_list = ignore_string.split(",")
+
 with open("sample.txt") as opened_file:
     book = opened_file.read().lower()
 
@@ -11,14 +22,16 @@ for p in string.punctuation:
 histogram = {}
 
 for word in book.split(" "):
-    if word in histogram.keys():
-        histogram[word] += 1
+    if word in ignore_list:
+        del word
     else:
-        histogram[word] = 1
+        if word in histogram.keys():
+            histogram[word] += 1
+        else:
+            histogram[word] = 1
 
 import operator
 sorted_hist = sorted(histogram.items(), key=operator.itemgetter(1))
 for idx, item in enumerate(sorted_hist[:-21:-1]):
     word, count = item
     print(idx + 1, word, count)
-#print(sorted_hist[:-21:-1])

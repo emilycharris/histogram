036a5545a30af243da309d35134825efe3d67c4b
emilycharris <emily.harris.mail@gmail.com>
2016-05-18 14:57:10 -0400

    completed sort

--- my_histogram.py
+++ my_histogram.py
@@ -1,12 +1,12 @@
 import string
 
 with open("sample.txt") as opened_file:
-    book = opened_file.read()
+    book = opened_file.read().lower()
 
 for p in string.punctuation:
     book = book.replace(p, "")
-    book = book.replace("/n", "")
-
+    book = book.replace("\n", " ")
+    book = book.replace("  "," ")
 
 histogram = {}
 
@@ -16,11 +16,6 @@ for word in book.split(" "):
     else:
         histogram[word] = 1
 
-for key, value in histogram.items():
-    print(key, value)
-
-#print(histogram)
-
-#hard mode - you have to sort smallest to largest occurence *you can sort a list, not dictionary
-#also to draw out the amount of # signs for that
-#string multiplication print("#" * 20)
+import operator
+sorted_hist = sorted(histogram.items(), key=operator.itemgetter(1))
+print(sorted_hist[:-20:-1])
